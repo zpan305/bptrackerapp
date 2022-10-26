e489d22aecdf6fe023a6d0a110596c2fec2a642a
pan00033 <zuxingp888@gmail.com>
2022-10-25 22:26:05 -0400

    adding logic for fetching patient data

--- src/app/patient/patient.component.ts
+++ src/app/patient/patient.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { EmailService } from '../shared/email.service';
 
@@ -10,10 +11,31 @@ import { EmailService } from '../shared/email.service';
 export class PatientComponent implements OnInit {
 
   patientEmail: string;
+  patientExist: boolean = true;
 
-  constructor(private emailService: EmailService) { }
+  constructor(private emailService: EmailService, private http: HttpClient) {}
 
   ngOnInit(): void {
     this.patientEmail = this.emailService.patientEmail;
+    if(this.patientEmail) {
+      localStorage.setItem('email', this.patientEmail);
+    } else{
+      this.patientEmail = localStorage.getItem('email');
+    }
+    this.fetchPatientData();
+  }
+
+  fetchPatientData() {
+    this.http.get('https://high-blood-pressure-tracker-default-rtdb.firebaseio.com/' + this.patientEmail)
+    .subscribe({
+      next: (v) => {
+        console.log(v);
+        this.patientExist = true;
+      },
+      error: (e) => {
+        this.patientExist = false;
+      },
+      complete: () => console.info('complete')
+    });
   }
 }
